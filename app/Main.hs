7e91ac1c7ec9f1cff8ab16bb35dfb04b72f5867b
Matthew Mercado <MatthewMerc@gmail.com>
2024-05-29 20:07:35 -0500

    up to chapter 2.5

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,6 @@ import Data.List
 -- Axiom 1
 data Surreal = Surreal {left :: Set.Set Surreal, right :: Set.Set Surreal}
 
--- (<|>) :: Surreal -> Surreal -> Surreal
-
 (<|>) :: Maybe Surreal -> Maybe Surreal -> Surreal
 (<|>) Nothing Nothing = Surreal empty empty
 (<|>) Nothing (Just r) = Surreal empty (Set.singleton r)
@@ -28,15 +26,12 @@ surOne :: Surreal
 surOne = Surreal (Set.singleton surZero) empty
 
 surNOne :: Surreal
-surNOne = Surreal empty (Set.singleton surZero)
-
+surNOne = -surOne
 
 -- Just figuring out function currying here mainly
 show' ::(Surreal -> Set.Set Surreal) -> Surreal -> [String]
 show' x = Set.toList . Set.map show . x
 
-instance Show Surreal where
-  show s = "{"  ++ intercalate "," (show' left s) ++ "|" ++ intercalate "," (show' right s) ++ "}"
 
 -- Axiom 2. One number is less than or equal to another number
 -- if and only if no member of the first number’s left set is greater than or equal to the second number,
@@ -47,27 +42,45 @@ instance Ord Surreal where
   x <= y =  not ( True `Set.member` Set.map (>= y) (left x))
       && not ( True `Set.member` Set.map (<= x) (right y))
 
+-- Definition of 'likeness' see Theorem 1.5
 instance Eq Surreal where
   (==) :: Surreal -> Surreal -> Bool
   x == y = x <= y && y <= x
 
+--Defining addition on a set
+
+add :: Set.Set Surreal -> Surreal -> Set.Set Surreal
+add x y = Set.map (+y) x
+
 
 
 instance Num Surreal where
+  -- Definition 2.1
+  negate :: Surreal -> Surreal
+  negate x
+    | x == surZero =  x
+    | otherwise = Surreal (Set.map negate (right x)) (Set.map negate (left x))
   (+) :: Surreal -> Surreal -> Surreal
   -- Definition 2.2. x + y = {XL + y, x + YL|XR + y, x + YR}
-  (+) x y = Surreal  (Set.union (Set.map (+x) (left y)) (Set.map (+y) (left x) )) (Set.union (Set.map (+x) (right y)) (Set.map (+y) (right x) ))
+  (+) x y = Surreal  (Set.union (left y `add` x) (left x `add` y )) (Set.union (right y `add` x) (right x `add` y))
   (-) :: Surreal -> Surreal -> Surreal
   -- Definition 2.3. x − y = x + (−y)
   (-) x y = x + negate y
-  -- TODO
+  -- Definition 2.4.
+  -- x·y = {XL·y+x·YL−XL·YL,XR·y+x·YR−XR·YR|XL·y+x·YR−XL·YR,XR·y+x·YL−XR·YL}.
   (*) :: Surreal -> Surreal -> Surreal
-  (*) = (+)
-  -- Definition 2.1
-  negate :: Surreal -> Surreal
-  negate s
-    | s == surZero =  s
-    | otherwise = Surreal (Set.map negate (right s)) (Set.map negate (left s))
+  (*) x y = let
+        sOp op s s' = Set.unions $ Set.map (\si -> Set.map (op si) s') s
+        mult = sOp (*)
+        addS = sOp (+)
+        subS = sOp subtract
+        multl s n = Set.map (* n) s
+        a = (left x `multl` y) `addS` (left y `multl` x) `subS` (left x `mult` left y)
+        b = (right x `multl` y) `addS` (right y `multl` x) `subS` (right x `mult` right y)
+        c = (left x `multl` y) `addS` (right y `multl` x) `subS` (left x `mult` right y)
+        d = (right x `multl` y) `addS` (left y `multl` x) `subS` (right x `mult` left y)
+    in
+        Surreal (Set.union a b) (Set.union c d)
   abs :: Surreal -> Surreal
   abs s
     | s < surZero = negate s
@@ -79,8 +92,8 @@ instance Num Surreal where
     | otherwise = surZero
   fromInteger :: Integer -> Surreal
   fromInteger n
-    | n > 0 = surOne + fromInteger(n-1)
-    | n < 0 = surNOne + fromInteger(n+1)
+    | n > 0 = surOne + fromInteger (n-1)
+    | n < 0 = surNOne + fromInteger (n+1)
     | otherwise = surZero
 
 
@@ -109,7 +122,9 @@ type Day = Int
 genSurreal :: Day -> Set.Set Surreal
 genSurreal d = Set.fromList $ iterate nextDay [surZero] !! d
 
+instance Show Surreal where
+  show s = "{"  ++ intercalate "," (show' left s) ++ "|" ++ intercalate "," (show' right s) ++ "}"
+
 main :: IO ()
 main = do
-    print (  surZero - surNOne)
-    -- print $ Surreal (Set.singleton surNOne) (Set.fromList [  surOne, surZero])
+  print ((surOne + surOne) * (surOne + surOne) )
